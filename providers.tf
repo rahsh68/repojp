a6ed7e4aaa1bb9c7a2587b6cef1cd79b6c69605a
Rahil S <rahilspk@gmail.com>
2022-12-21 13:14:25 +0100

    Commiting first git code

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "1.3.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
+    }
+  }
+}
+provider "aws" {
+  region = "eu-north-1"
+}
